05a6e3c9da8422611da14d272e1d0479fb6f92bd
yangqian_WK <yangqian059@163.com>
2021-09-28 14:41:44 +0800

    fix carousel figure

--- Assets/Scripts/CarouselFigure/CarouselFigure.cs
+++ Assets/Scripts/CarouselFigure/CarouselFigure.cs
@@ -28,7 +28,9 @@ public class CarouselFigure : ScrollRect
     private int tarIndex;
     private bool draging;
     private float startLocalX;
+    private Vector3 UIPos = Vector3.zero;
     private float offset;
+    private float viewPortWidth;
     public event Action<int> OnContentChanged;
 
     protected override void Awake()
@@ -42,15 +44,19 @@ public class CarouselFigure : ScrollRect
         this.fixedPosArr = new float[contentCount];
         this.toggleArr = toggleGroup.GetComponentsInChildren<Toggle>();
         this.contentGrid = content.GetComponent<GridLayoutGroup>();
-        this.contentGrid.padding.left = (int)(viewport.rect.width - contentGrid.cellSize.x) / 2;
+        contentGrid.GetComponent<RectTransform>().pivot = Vector2.up * 0.5f;
+        contentGrid.GetComponent<RectTransform>().anchorMin = Vector2.up * 0.5f;
+        contentGrid.GetComponent<RectTransform>().anchorMax = Vector2.up * 0.5f;
+        viewPortWidth = viewport.rect.width;
+        this.contentGrid.padding.left = (int)(viewPortWidth - contentGrid.cellSize.x) / 2;
         this.contentGrid.spacing = Vector2.right * contentGrid.padding.left;
         this.totalSpacingX = contentGrid.cellSize.x + contentGrid.spacing.x;
         for (int i = 0; i < contentCount; i++)
         {
-            fixedPosArr[i] = i * totalSpacingX + content.localPosition.x;
-
+            fixedPosArr[i] = i * totalSpacingX;
+            Debug.LogError("fixedPosArr[i]" + fixedPosArr[i]);
         }
-
+        Debug.LogError("contentLen" + contentLen);
         this.contentLen = totalSpacingX * contentCount + contentGrid.padding.left + contentGrid.padding.right;
         this.totalSpacingXRate = totalSpacingX / contentLen;
         this.content.sizeDelta = new Vector2(contentLen, content.sizeDelta.y);
@@ -68,13 +74,18 @@ public class CarouselFigure : ScrollRect
         InitBound();
     }
 
+    
 
     public override void OnBeginDrag(PointerEventData eventData)
     {
         base.OnBeginDrag(eventData);
+
         draging = true;
         startLocalX = content.transform.localPosition.x;
+        Debug.LogError("OnBeginDrag startLocalX:" + startLocalX);
     }
+
+
 
     public override void OnEndDrag(PointerEventData eventData)
     {
@@ -85,64 +96,72 @@ public class CarouselFigure : ScrollRect
         offset = content.transform.localPosition.x - startLocalX;
         if (tarIndex == curIndex)
         {
-            //Debug.LogError("offset: " + offset + "-spacingX /2: " + totalSpacingX * CtrticalRate);
-            //Debug.LogError("velocity:" + velocity.x);
+            Debug.LogError("offset: " + offset + "--totalSpacingX * CRITICAL_RATE: " + totalSpacingX * CRITICAL_RATE);
+            Debug.LogError("velocity:" + velocity.x);
             if (offset > totalSpacingX * CRITICAL_RATE)
             {
-                //Debug.LogError("offset > totalSpacingX * CtrticalRate");
+                Debug.LogError("offset > totalSpacingX * CtrticalRate");
                 MoveToPos(tarIndex - 1);
                 return;
             }
             if (offset < -totalSpacingX * CRITICAL_RATE)
             {
-                //Debug.LogError("offset < -totalSpacingX * CtrticalRate");
+                Debug.LogError("offset < -totalSpacingX * CtrticalRate");
                 MoveToPos(tarIndex + 1);
                 return;
             }
 
             if (velocity.x > CRITICAL_VELOCITY)
             {
-                //Debug.LogError("velocity.x > VelocityCtrtical");
+                Debug.LogError("velocity.x > VelocityCtrtical");
                 MoveToPos(tarIndex - 1);
                 return;
             }
             if (velocity.x < -CRITICAL_VELOCITY)
             {
-                //Debug.LogError("velocity.x < -VelocityCtrtical");
+                Debug.LogError("velocity.x < -VelocityCtrtical");
                 MoveToPos(tarIndex + 1);
                 return;
             }
 
             if (!FloatEquals(content.localPosition.x, fixedPosArr[curIndex]))
             {
-                //Debug.LogError("!FloatEquals(content.localPosition.x, fixedPosArr[curIndex])");
+                Debug.LogError("!FloatEquals(content.localPosition.x, fixedPosArr[curIndex])");
                 MoveToPos(tarIndex);
             }
 
         }
         else if (tarIndex < curIndex)
         {
-            //Debug.LogError("tarIndex < curIndex");
+            Debug.LogError("tarIndex < curIndex");
             MoveToPos(curIndex - 1);
         }
         else
         {
-            //Debug.LogError("tarIndex > curIndex");
+            Debug.LogError("tarIndex > curIndex");
             MoveToPos(curIndex + 1);
         }
     }
+
+    private void GetUIPos(Vector3 pos,ref Vector3 uiPos)
+    {
+        uiPos.x = pos.x - Screen.width / 2;
+        uiPos.y = pos.y - Screen.height / 2;
+    }
+
     public override void OnDrag(PointerEventData eventData)
     {
-        if (eventData.position.x < viewPortLeft)
+        GetUIPos(eventData.position, ref UIPos);
+        Debug.LogError("UIPos.position" + UIPos);
+
+        if (UIPos.x < viewPortLeft)
         {
             OnEndDrag(eventData);
-            //MoveToPos(content.position.x - viewPortLeft);
             return;
         }
-        if (eventData.position.x > viewPortRight)
+        if (UIPos.x > viewPortRight)
         {
             OnEndDrag(eventData);
-            //MoveToPos(content.position.x - viewPortLeft);
             return;
         }
         base.OnDrag(eventData);
@@ -162,7 +181,7 @@ public class CarouselFigure : ScrollRect
             index = 0;
         if (index >= contentCount)
             index = contentCount - 1;
-        //Debug.LogError("MoveTo:" + index + "-" + fixedPosArr[index]);
+        Debug.LogError("MoveTo:" + index + "-" + fixedPosArr[index]);
         velocity = Vector2.zero;
         content.DOLocalMove(GetPos(index), MOVE_TIME).onComplete = () =>
         {
@@ -180,7 +199,25 @@ public class CarouselFigure : ScrollRect
 
     public void InitBound()
     {
-        viewPortLeft = viewport.position.x - viewport.pivot.x * viewport.rect.width;
-        viewPortRight = viewport.position.x + (1 - viewport.pivot.x) * viewport.rect.width;
+        viewPortLeft = viewport.localPosition.x - viewport.pivot.x * viewPortWidth;
+        viewPortRight = viewport.localPosition.x + (1 - viewport.pivot.x) * viewPortWidth;
+        Debug.LogError("viewPortLeft" + viewPortLeft);
+        Debug.LogError("viewPortRight" + viewPortRight);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            MoveToPos(0);
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            MoveToPos(1);
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            MoveToPos(2);
+        }
     }
 }
