1924e5a2fef20d933bc713a18e679c22769ddaa4
yangqian_WK <yangqian059@163.com>
2021-09-28 11:38:41 +0800

    delete annotation

--- Assets/Editor/CarouselFigureEditor.cs
+++ Assets/Editor/CarouselFigureEditor.cs
@@ -1,17 +1,12 @@
 ﻿using UnityEngine;
 using UnityEditor;
 
-//CustomEditor 自定义编辑器
-//描述了用于编辑器实时运行类型的一个编辑器类。
-//注意：这是一个编辑器类，如果想使用它你需要把它放到工程目录下的Assets/Editor文件夹下。
-//编辑器类在UnityEditor命名空间下。所以当使用C#脚本时，你需要在脚本前面加上 "using UnityEditor"引用。
 [CustomEditor(typeof(CarouselFigure), true)]
 [CanEditMultipleObjects]
 public class CarouselFigureEditor : UnityEditor.UI.ScrollRectEditor
 {
     SerializedProperty Toggles;
     SerializedProperty contentCount;
-    //SerializedProperty _segements;
 
     protected override void OnEnable()
     {
